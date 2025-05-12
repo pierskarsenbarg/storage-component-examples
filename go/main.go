383913cf1a2691da7abf3e8ec7cbea2ef4d3cb89
Piers Karsenbarg <piers@pulumi.com>
2025-05-12 17:37:08 +0100

    added go version

---
+++ go/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	storage "github.com/pierskarsenbarg/storage/sdk"
+
+	"github.com/pulumi/pulumi-azure-native-sdk/resources/v2"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		// Create an Azure Resource Group
+		resourceGroup, err := resources.NewResourceGroup(ctx, "resourceGroup", nil)
+		if err != nil {
+			return err
+		}
+
+		_, err = storage.NewStorage(ctx, "mystorage", &storage.StorageArgs{
+			ResourceGroupName: resourceGroup.Name,
+		})
+
+		return nil
+	})
+}
